dfa86806c0c5d415dadc954710c54bc4a26b5b88
edurtra0110 <151035916+edurtra0110@users.noreply.github.com>
2024-02-09 18:20:24 +0100

    Add files via upload

---
+++ ejercicio 7/js/script.js
@@ -0,0 +1,12 @@
+function Comprobar(){
+    let numero;
+    numero = parseInt(prompt("¿Qué nota tienes?"), 10);
+    if (numero>=0 && numero<5) {
+       alert ("Estas suspenso")
+    }
+      else { 
+      alert ("Estas aprobado")
+      }
+  }
+
+
