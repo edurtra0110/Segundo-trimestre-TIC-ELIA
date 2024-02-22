d07c9c501e8462dded2a6835c6bc1f7c5e18c6e7
edurtra0110 <151035916+edurtra0110@users.noreply.github.com>
2024-02-22 14:19:43 +0100

    Delete ejercicio 3 directory

--- ejercicio 3/js/script.js
+++
@@ -1,6 +0,0 @@
-function pedirDatos(){
-	let nombre, edad;
-  nombre = prompt("¿Cómo te llamas?");
-  edad= prompt("¿Cuántos años tienes?");
-  alert("Hola " + nombre + ",veo que tienes " + edad);
-}
