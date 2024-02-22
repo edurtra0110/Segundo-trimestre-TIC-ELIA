5a6514e9b4c14c06f51e01d6bbd0929bd0ec8903
edurtra0110 <151035916+edurtra0110@users.noreply.github.com>
2024-02-22 14:06:31 +0100

    Add files via upload

---
+++ ejercicio 10/js/script.js
@@ -0,0 +1,8 @@
+function Calificar() {
+    let alumno = ["Elia", "Claudia", "Julia", "Rocio"];
+    let notas = [];
+    notas[0] = parseInt(prompt("¿Qué nota le pones a "+ alumno[0]), 10);
+    notas[1] = parseInt(prompt("¿Qué nota le pones a "+ alumno[1]),10);
+    notas[2] = parseInt(prompt("¿Qué nota le pones a" + alumno[2]),10);
+    notas[3] = parseInt(prompt("¿Qué nota le pones a " + alumno[3]),10);
+}
